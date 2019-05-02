1365de315d7e58967f3f89613f68dbd9da49a3db
cole <xdxlwx@gmail.com>
2019-05-02 23:18:06 +1000

    setup required libraries and redux environment

---
+++ src/reducers/initialState.js
@@ -0,0 +1,6 @@
+export default {
+  /* structure: { userId: idStr, firstName: firstNameStr, lastName: lastNameStr, 
+                  dateOfBirth: dobStr, email: emailStr, mobileNum: number, password: pwdStr} 
+   */
+  user: {}
+};
