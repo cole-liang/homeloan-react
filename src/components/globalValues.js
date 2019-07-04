24a36c391799e0ed8d9cc086c66f4568ca7e60c5
cole <xdxlwx@gmail.com>
2019-07-04 16:52:45 +1000

    name changed

--- src/components/globalValues.js
+++ src/components/globalValues.js
@@ -2,4 +2,4 @@ export const BACKGROUND_URL =
   "http://www.zinqsolutions.com.au/wp-content/uploads/2018/12/zinq-bg.png";
 export const COMPANY_LOGO_URL =
   "http://www.zinqsolutions.com.au/wp-content/uploads/2018/12/zinq-comingsoon-logo2.png";
-export const COMPANY_NAME = "Zinq Solutions";
+export const COMPANY_NAME = "Zuqo Solutions";
